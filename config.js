cc79ae077f1e1cb4a3d9a7f3243ebc5df79132ef
Jason Vance <=>
2018-12-11 18:51:20 -0600

    init commit

---
+++ config.js
@@ -0,0 +1,15 @@
+'use strict';
+let databaseUrl = '';
+if (process.env.NODE_ENV === 'test') {
+    databaseUrl = process.env.TEST_DATABASE_URL;
+} else {
+    databaseUrl = process.env.DATABASE_URL;
+}
+// exports.DATABASE_URL =
+//     process.env.DATABASE_URL ||
+//     global.DATABASE_URL ||
+//     'mongodb://localhost/jwt-auth-demo';
+exports.DATABASE_URL = databaseUrl;
+exports.PORT = process.env.PORT || 8080;
+exports.JWT_SECRET = process.env.JWT_SECRET;
+exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d';
---
+++ server.js
@@ -0,0 +1,79 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const passport = require('passport');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const { router: usersRouter } = require('./users');
+const { router: authRouter, localStrategy, jwtStrategy } = require('./auth');
+
+let { PORT, DATABASE_URL } = require('./config');
+if (process.env.NODE_ENV === 'test') {
+  DATABASE_URL = "mongodb://localhost/jwt-auth-demo";
+}
+
+const app = express();
+
+// App Setup
+app.use(morgan('combined'));
+// CORS
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
+  if (req.method === 'OPTIONS') {
+    return res.send(204);
+  }
+  next();
+});
+
+const jwtAuth = passport.authenticate('jwt', { session: false });
+
+passport.use(localStrategy);
+passport.use(jwtStrategy);
+
+app.use('/api/auth/', authRouter);
+app.use('/api/users/', usersRouter);
+
+let server;
+
+function runServer() {
+    return new Promise((resolve, reject) => {
+  
+      mongoose.connect(DATABASE_URL, { useNewUrlParser: true}, err => {
+        if (err) {
+          return reject(err);
+        }
+        server = app
+          .listen(PORT, () => {
+            console.log(`Your app is listening on port ${PORT}`);
+            resolve();
+          })
+          .on('error', err => {
+            mongoose.disconnect();
+            reject(err);
+          });
+      });
+    });
+  }
+  
+  function closeServer() {
+    return mongoose.disconnect().then(() => {
+      return new Promise((resolve, reject) => {
+        console.log('Closing server');
+        server.close(err => {
+          if (err) {
+            return reject(err);
+          }
+          resolve();
+        });
+      });
+    });
+  }
+  
+  if (require.main === module) {
+    runServer().catch(err => console.error(err));
+  }
+  
+  module.exports = { app, runServer, closeServer };
